6c9ef9d27f9e38e7cf8b814cc79b400299621d7d
Shunta Komatsu <shuntak217@gmail.com>
2020-12-17 12:38:50 +0900

    feat: add some bg images

--- bin/lock.sh
+++ bin/lock.sh
@@ -3,7 +3,7 @@
 i3lock \
   --nofork \
   --no-unlock-indicator \
-  --image="$DOTFILES/images/bg-lock.jpg" \
+  --image="$DOTFILES/images/bg/lock.jpg" \
   --ignore-empty-password \
   --tiling \
   \
