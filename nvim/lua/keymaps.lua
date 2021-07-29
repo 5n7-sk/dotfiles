a794a8d576e715b8b6ce068aa6045789dae65f1b
Shunta Komatsu <skomatsu@pm.me>
2021-07-30 00:45:18 +0900

    feat: add keymaps to resize split windows

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -9,13 +9,19 @@ map("n", "gk", "k")
 
 map("n", "$", "$l")
 
+map("n", "sh", "<c-w>s")
+map("n", "sv", "<c-w>v")
+
 map("n", "<c-h>", "<c-w>h")
 map("n", "<c-j>", "<c-w>j")
 map("n", "<c-k>", "<c-w>k")
 map("n", "<c-l>", "<c-w>l")
 
-map("n", "sh", "<c-w>s")
-map("n", "sv", "<c-w>v")
+-- resize windows
+map("n", "<m-H>", "<cmd>vertical resize -10<cr>")
+map("n", "<m-J>", "<cmd>resize +5<cr>")
+map("n", "<m-K>", "<cmd>resize -5<cr>")
+map("n", "<m-L>", "<cmd>vertical resize +10<cr>")
 
 -- kill line without copying
 map("n", "<m-k>", "\"_dd")
