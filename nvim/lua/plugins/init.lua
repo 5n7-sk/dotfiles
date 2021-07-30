883a6fc04a5147262fab63136da0c5ae377c6749
Shunta Komatsu <skomatsu@pm.me>
2021-07-30 12:30:53 +0900

    feat: add formatter using prettier

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -45,10 +45,14 @@ return require("packer").startup {
         require("format").setup {
           ["*"] = {{cmd = {"sed -i 's/[ \t]*$//'"}}},
           bash = {{cmd = {"shfmt -i 2 -w"}}},
+          javascript = {{cmd = {"prettier -w"}}},
+          json = {{cmd = {"prettier -w"}}},
           go = {{cmd = {"goimports -w"}}},
           lua = {{cmd = {"lua-format -i"}}},
+          markdown = {{cmd = {"prettier -w"}}},
           python = {{cmd = {"black --quiet", "isort"}}},
           sh = {{cmd = {"shfmt -i 2 -w"}}},
+          typescript = {{cmd = {"prettier -w"}}},
           zsh = {{cmd = {"shfmt -i 2 -w"}}}
         }
       end
