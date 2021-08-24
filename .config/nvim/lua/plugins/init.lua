ddf15722e1544ec3266ca2c81c3b4d99ac222522
Shunta Komatsu <skomatsu@pm.me>
2021-08-24 17:20:35 +0900

    refactor: remove unused lines

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -314,7 +314,6 @@ return require("packer").startup {
         local map = require("utils").map
         map("n", "<leader>fb", "<cmd>DashboardJumpMarks<cr>")
         map("n", "<leader>tc", "<cmd>DashboardChangeColorscheme<cr>")
-        -- map("n", "<leader>ff", "<cmd>DashboardFindFile<cr>")
         map("n", "<leader>fh", "<cmd>DashboardFindHistory<cr>")
         map("n", "<leader>fa", "<cmd>DashboardFindWord<cr>")
         map("n", "<leader>cn", "<cmd>DashboardNewFile<cr>")
