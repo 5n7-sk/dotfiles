50e25d33fe0f6538d4e14d2487752fcabc1c3931
Shunta Komatsu <skomatsu@pm.me>
2021-09-07 18:38:53 +0900

    perf: disable some build-in plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -23,6 +23,32 @@ vim.opt.mouse = "a"
 
 vim.opt.timeoutlen = 300
 
+-- disable built-in plugins
+local disable_buildtin_plugins = {
+  "2html_plugin",
+  "getscript",
+  "getscriptPlugin",
+  "gzip",
+  "logipat",
+  "matchit",
+  "netrw",
+  "netrwFileHandlers",
+  "netrwPlugin",
+  "netrwSettings",
+  "rrhelper",
+  "spellfile_plugin",
+  "tar",
+  "tarPlugin",
+  "vimball",
+  "vimballPlugin",
+  "zip",
+  "zipPlugin"
+}
+
+for _, plugin in pairs(disable_buildtin_plugins) do
+  vim.g["loaded_" .. plugin] = 1
+end
+
 -- install packer.nvim if not exists
 local path = vim.fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
 
