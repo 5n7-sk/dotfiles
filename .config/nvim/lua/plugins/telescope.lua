2c17cb32389caf2807222f5c61a2fdfccf757ba9
Shunta Komatsu <shuntak217@gmail.com>
2021-09-16 02:32:28 +0900

    fix: also ignore subdirectories

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,10 +7,10 @@ M.config = function()
         "rg",
         "--color=never",
         "--column",
-        "--glob=!.git/**",
-        "--glob=!.venv/**",
-        "--glob=!__pycache__/**",
-        "--glob=!node_modules/**",
+        "--glob=!**/.git/**",
+        "--glob=!**/.venv/**",
+        "--glob=!**/__pycache__/**",
+        "--glob=!**/node_modules/**",
         "--hidden",
         "--line-number",
         "--no-heading",
@@ -25,7 +25,7 @@ M.setup = function()
   local map = require("utils").map
   map("n", "<c-p>", "<cmd>Telescope commands<cr>")
   map("n", "<leader>ff",
-    "<cmd>Telescope find_files find_command=rg,--files,--glob=!.git/**,--glob=!.venv/**,--glob=!__pycache__/**,--glob=!node_modules/**,--hidden,--no-ignore<cr>")
+    "<cmd>Telescope find_files find_command=rg,--files,--glob=!**/.git/**,--glob=!**/.venv/**,--glob=!**/__pycache__/**,--glob=!**/node_modules/**,--hidden,--no-ignore<cr>")
   map("n", "<leader>lg", "<cmd>Telescope live_grep<cr>")
   map("n", "<c-m>", "<cmd>Telescope lsp_workspace_diagnostics<cr>")
 end
