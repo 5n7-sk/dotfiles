86afc887d003bca6c2211a2bc0b6dc19b6c4ed0d
Shunta Komatsu <shuntak217@gmail.com>
2022-02-08 17:12:41 +0900

    refactor: refactor zeno config

--- zsh/zeno.zsh
+++ zsh/zeno.zsh
@@ -1,6 +1,5 @@
-export ZENO_ENABLE_FZF_TMUX=1
-export ZENO_FZF_COMMAND="fzf --layout reverse"
 export ZENO_ENABLE_SOCK=1
+export ZENO_FZF_COMMAND="fzf --layout reverse"
 
 if [[ -n $ZENO_LOADED ]]; then
   bindkey ' ' zeno-auto-snippet
