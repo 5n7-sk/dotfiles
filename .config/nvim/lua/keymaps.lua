baefe433150779fddec64f1ae0a7d1784dc95889
Shunta Komatsu <shuntak217@gmail.com>
2022-02-16 23:13:54 +0900

    refactor: use `vim.keymap.set` instead of original `map` function

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,43 +1,39 @@
-local map = require("utils").map
+vim.keymap.set("i", "<esc>", "<esc>l")
+vim.keymap.set("i", "jj", "<esc>l")
 
-map("i", "<esc>", "<esc>l")
-map("i", "jj", "<esc>l")
-
-map("n", "j", "gj")
-map("n", "k", "gk")
-map("n", "gj", "j")
-map("n", "gk", "k")
+vim.keymap.set("n", "j", "gj")
+vim.keymap.set("n", "k", "gk")
+vim.keymap.set("n", "gj", "j")
+vim.keymap.set("n", "gk", "k")
 
 -- cursor behaivor for virtualedit
 -- recuired: set virtualedit and whichwrap properly
-function _G.smart_h()
+vim.keymap.set("n", "h", function()
   return string.len(vim.fn.getline(vim.fn.line(".") - 1)) > 0 and vim.fn.col(".") == 1 and "hl" or "h"
-end
-map("n", "h", "v:lua.smart_h()", { expr = true })
+end, { expr = true })
 
-map("n", "$", "$l")
+vim.keymap.set("n", "$", "$l")
 
-map("n", "sh", "<c-w>s")
-map("n", "sv", "<c-w>v")
+vim.keymap.set("n", "sh", "<c-w>s")
+vim.keymap.set("n", "sv", "<c-w>v")
 
-map("n", "<c-h>", "<c-w>h")
-map("n", "<c-j>", "<c-w>j")
-map("n", "<c-k>", "<c-w>k")
-map("n", "<c-l>", "<c-w>l")
+vim.keymap.set("n", "<c-h>", "<c-w>h")
+vim.keymap.set("n", "<c-j>", "<c-w>j")
+vim.keymap.set("n", "<c-k>", "<c-w>k")
+vim.keymap.set("n", "<c-l>", "<c-w>l")
 
 -- resize windows
-map("n", "<m-H>", "<cmd>vertical resize -10<cr>")
-map("n", "<m-J>", "<cmd>resize +5<cr>")
-map("n", "<m-K>", "<cmd>resize -5<cr>")
-map("n", "<m-L>", "<cmd>vertical resize +10<cr>")
+vim.keymap.set("n", "<m-H>", "<cmd>vertical resize -10<cr>")
+vim.keymap.set("n", "<m-J>", "<cmd>resize +5<cr>")
+vim.keymap.set("n", "<m-K>", "<cmd>resize -5<cr>")
+vim.keymap.set("n", "<m-L>", "<cmd>vertical resize +10<cr>")
 
 -- kill line without copying
-map("n", "<m-k>", '"_dd')
-map("v", "<m-k>", '"_dd')
+vim.keymap.set({ "n", "v" }, "<m-k>", '"_dd')
 
-map("n", "<c-t>n", "<cmd>tabnew<cr>")
+vim.keymap.set("n", "<c-t>n", "<cmd>tabnew<cr>")
 
-map("n", "<leader>q", "<cmd>q!<cr>")
-map("n", "<leader>w", "<cmd>wa<cr>")
+vim.keymap.set("n", "<leader>q", "<cmd>q!<cr>")
+vim.keymap.set("n", "<leader>w", "<cmd>wa<cr>")
 
-map("n", "<esc>", "<cmd>nohlsearch<cr>")
+vim.keymap.set("n", "<esc>", "<cmd>nohlsearch<cr>")
