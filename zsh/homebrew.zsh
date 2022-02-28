9c3ffaa593310d0222f80e404d08c7fef9b9f861
Shunta Komatsu <shuntak217@gmail.com>
2022-02-28 23:24:43 +0900

    fix: source asdf

--- zsh/homebrew.zsh
+++ zsh/homebrew.zsh
@@ -2,3 +2,7 @@ if hash "brew" >/dev/null 2>&1; then
   fpath+="$(brew --prefix)/share/zsh/site-functions"
   autoload -Uz compinit && compinit
 fi
+
+if hash "asdf" >/dev/null 2>&1; then
+  . $(brew --prefix asdf)/libexec/asdf.sh
+fi
