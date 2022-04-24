62fe2cb5341bc9dcd1f12c44512fddbc96947d3c
Shunta Komatsu <shuntak217@gmail.com>
2022-04-24 20:46:07 +0900

    fix: fix i3lock option changes

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env sh
 
 i3lock \
-  --nofork \
-  --no-unlock-indicator \
-  --image="$DOTFILES/images/calvin-and-hobbes.jpg" \
-  --ignore-empty-password \
-  --tiling \
-  \
-  --clock \
-  --pass-media-keys \
-  --pass-screen-keys \
-  --pass-power-keys \
-  --pass-volume-keys \
-  --indpos="w-540:h-400" \
-  --timecolor=2e3440ff \
-  --timestr="%H:%M:%S" \
-  --timepos="ix:iy+200" \
-  --time-font="Calvin and Hobbes" \
-  --timesize=140 \
-  --datecolor=2e3440ff \
-  --datestr="%A, %d %B" \
-  --datepos="tx:ty+120" \
-  --date-font="Calvin and Hobbes" \
-  --datesize=100
+	--nofork \
+	--no-unlock-indicator \
+	--image="$DOTFILES/images/calvin-and-hobbes.jpg" \
+	--ignore-empty-password \
+	--tiling \
+	\
+	--clock \
+	--pass-media-keys \
+	--pass-screen-keys \
+	--pass-power-keys \
+	--pass-volume-keys \
+	--ind-pos="w-540:h-400" \
+	--time-color=2e3440ff \
+	--time-str="%H:%M:%S" \
+	--time-pos="ix:iy+200" \
+	--time-font="Calvin and Hobbes" \
+	--time-size=140 \
+	--date-color=2e3440ff \
+	--date-str="%A, %d %B" \
+	--date-pos="tx:ty+120" \
+	--date-font="Calvin and Hobbes" \
+	--date-size=100
